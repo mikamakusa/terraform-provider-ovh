6c79ad66a91461954a4fb06169532bd58e66bed7
Radek Goblin Pieczonka <goblin@pentex.pl>
2016-12-27 23:02:07 +0100

    adapt package name

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/kuachi/terraform-provider-ovh/ovh"
+	"github.com/goblain/terraform-provider-ovh/ovh"
 )
 
 func main() {
